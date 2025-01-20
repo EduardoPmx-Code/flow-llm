3e69312c4470fdc344f9e7d97f18463fd50770e5
Eduardopmx <eduardop.29.91@gmail.com>
2025-01-20 13:59:03 -0300

    add status ia by axios and vercel

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,55 @@
 import { Injectable } from '@nestjs/common';
+import { generateText } from 'ai';
+import { createOllama, ollama } from 'ollama-ai-provider';
+import { HttpService } from '@nestjs/axios';
+import { firstValueFrom } from 'rxjs';
+import { IaProvider } from './providers/provider-is';
 
 @Injectable()
 export class AppService {
+  private Model: any;
+  constructor(private readonly httpService: HttpService) {
+    const llamaServer = new IaProvider();
+    llamaServer.getOllamaClient();
+    this.Model = llamaServer.getModel();
+  }
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async statusServer(): Promise<any> {
+    try {
+      const response = await generateText({
+        model: this.Model,
+        prompt: 'say hello',
+      });
+      console.log(response);
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async requestServerAxios() {
+    try {
+      const response = await firstValueFrom(
+        this.httpService.post(
+          'http://ollama:11434/api/generate',
+          {
+            model: 'llama3',
+            prompt: 'Why is the sky blue?',
+          },
+          {
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          },
+        ),
+      );
+
+      console.log('Respuesta de Ollama:', response.data);
+    } catch (error) {
+      console.error('Error al hacer la solicitud:', error);
+    }
   }
 }
