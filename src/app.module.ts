ff834e2f7404004a7661d7d2ad1718b8251fefc9
Eduardopmx <eduardop.29.91@gmail.com>
2025-01-20 13:57:57 -0300

    add module axios http

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SocketIaModule } from './socket-ia/socket-ia.module';
 import { LayersIaModule } from './layers-ia/layers-ia.module';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [SocketIaModule],
+  imports: [SocketIaModule, HttpModule],
   controllers: [AppController],
   providers: [AppService],
 })
